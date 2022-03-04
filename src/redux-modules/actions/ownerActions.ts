7685b542de264d240eb1b31a28d0ae69f947e282
Israel Heiblum <i.heiblum@gmail.com>
2022-03-04 12:50:54 +0200

    test

--- src/redux-modules/actions/ownerActions.ts
+++ src/redux-modules/actions/ownerActions.ts
@@ -3,6 +3,7 @@ import {OwnerActionTypes} from "../action-types/ownerActionTypes";
 
 
 export const setOwnerList = (ownerList: any) => {
+    // test
     return {
         type: OwnerActionTypes.SET_OWNER_LIST,
         payload: ownerList
